b92b5e71ec6abb68124d4613792052e872fb971e
Calil-Silva <ka.renner@hotmail.com>
2021-11-19 07:44:56 -0300

    Fixed heroku url

--- src/services/gratibox.js
+++ src/services/gratibox.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URL =
   process.env.NODE_ENV === "dev"
     ? "http://localhost:4000"
-    : "https://gratibox-webapp.herokuapp.com/";
+    : "https://gratibox-webapp.herokuapp.com";
 
 function setConfig(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
